b2ee589b72acdeb5c37b8202fa0b0717772747e5
TANG QIAN <tangqiansmile@gmail.com>
2025-10-29 11:51:30 +0900

    Add files via upload

--- Rock_paper_Scissors_game.py
+++ Rock_paper_Scissors_game.py
@@ -3,16 +3,19 @@ import random
 emojis = {'r':'rock','p':'paper','s':'scissors'}
 game_letters = ['r','p','s']
 
-while True:
-    user_choice = input("enter rock or paper or scissor by 'r' or 'p' or 's'").lower()
-    if user_choice not in game_letters:
-        print(f"error, invalide choice")
-        continue #jump back to while loop
+def get_user_choice():
+    while True:
+        user_choice = input("enter rock or paper or scissor by 'r' or 'p' or 's'").lower()
+        if user_choice in game_letters:
+            return user_choice
+        else:
+            print(f"error, invalide choice")
 
-    computer_choice = random.choice(game_letters)
+def display_choices(user_choice, computer_choice):
+    print(f"you chose:{emojis[user_choice]}")
+    print(f"computer chose:{emojis[computer_choice]}")
 
-    print(f"you chose:{emojis[user_choice]}, computer chose:{emojis[computer_choice]}")
-
+def determin_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Tie, it is a draw!")
     elif(
@@ -22,9 +25,33 @@ while True:
         print("computer win!")  
     else:
         print("you win!")
+
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(game_letters)
+
+        display_choices(user_choice, computer_choice)
+
+        determin_winner(user_choice, computer_choice)
+        
+        user_decision = input("do you want to continue the game? answer by y or n").lower()
+        if user_decision == "n":
+            print(f"let's play in next time")
+            break 
+        
+play_game()
+        
+
        
-    user_decision = input("do you want to continue the game? answer by y or n").lower()
-    if user_decision == "n":
-        print(f"let's play in next time")
-        break
+
+
+
+
+
+    
+    
+
+    
     
