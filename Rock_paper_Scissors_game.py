ad593b594e7f2d45cb5da1ec99c85478930a283b
TANG QIAN <tangqiansmile@gmail.com>
2025-10-30 18:54:42 +0900

    Add files via upload

--- Rock_paper_Scissors_game.py
+++ Rock_paper_Scissors_game.py
@@ -1,12 +1,16 @@
 import random
 
-emojis = {'r':'rock','p':'paper','s':'scissors'}
-game_letters = ['r','p','s']
+rock = 'r'
+scissors = 's'
+paper = 'p'
+
+emojis = {rock:'rock',paper:'paper',scissors:'scissors'}
+choices = tuple(emojis.key())
 
 def get_user_choice():
     while True:
         user_choice = input("enter rock or paper or scissor by 'r' or 'p' or 's'").lower()
-        if user_choice in game_letters:
+        if user_choice in choices:
             return user_choice
         else:
             print(f"error, invalide choice")
@@ -19,9 +23,9 @@ def determin_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Tie, it is a draw!")
     elif(
-        (user_choice == 'r' and computer_choice == 'p') or
-        (user_choice == 'p' and computer_choice == 's') or
-        (user_choice == 's' and computer_choice == 'r')):
+        (user_choice == rock and computer_choice == paper) or
+        (user_choice == paper and computer_choice == scissors) or
+        (user_choice == scissors and computer_choice == rock)):
         print("computer win!")  
     else:
         print("you win!")
@@ -35,7 +39,7 @@ def play_game():
         display_choices(user_choice, computer_choice)
 
         determin_winner(user_choice, computer_choice)
-        
+
         user_decision = input("do you want to continue the game? answer by y or n").lower()
         if user_decision == "n":
             print(f"let's play in next time")
